4c73526d3c4f3815dcb0da5b605df77df98409d2
Vanajakshi12 <117273180+Vanajakshi12@users.noreply.github.com>
2024-11-25 08:31:54 +0530

    Add files via upload

---
+++ Exp5/src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'rexp5';
+
+  list: any[] = [];
+
+  addTask(item: string) {
+    this.list.push({ id: this.list.length, name: item });
+    console.warn(this.list);
+  }
+
+  removeTask(id: number) {
+    console.warn(id);
+    this.list = this.list.filter(item => item.id !== id);
+  }
+}
+
+
